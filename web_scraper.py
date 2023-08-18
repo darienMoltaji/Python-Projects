f872353235df2abb3c52d5f3f5566b26a2e9185f
darienMoltaji <d.moltaji@gmail.com>
2023-08-18 11:35:22 -0700

    Creating a Webscraper
    Webscrapes a NY Times article for all the links in the website

---
+++ web_scraper.py
@@ -0,0 +1,23 @@
+import json
+import requests
+from bs4 import BeautifulSoup
+
+def scrape_hyperlinks():
+    url = 'https://static.nytimes.com/email-content/RD_sample.html?action=click&module=nl-index-see-the-latest'
+    response = requests.get(url)
+    results = {"hyperlinks": []}
+    if response.status_code == 200:
+        soup = BeautifulSoup(response.content, 'html.parser')
+        hyperlinks = soup.find_all('a')
+
+        for hyperlink in hyperlinks:
+            hyperlinks_text = hyperlink.attrs['href']
+            results['hyperlinks'].append(hyperlinks_text)
+            # print(f"{hyperlinks_text}\n")
+        with open("hyperlinks.json", "w") as file:
+            file.write(json.dumps(results))
+    else:
+        print("Failed to retrieve data from the website.")
+
+if __name__ == "__main__":
+    scrape_hyperlinks()
