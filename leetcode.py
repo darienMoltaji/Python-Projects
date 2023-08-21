121cb69a92b37cff9a8a343bb54fc4099f7abbf4
darienMoltaji <d.moltaji@gmail.com>
2023-08-21 10:28:20 -0700

    Creates a roman numeral to int converter

---
+++ leetcode.py
@@ -0,0 +1,43 @@
+# palindrome number
+class Solution(object):
+    def isPalindrome(self, x):
+        """
+        :type x: int
+        :rtype: bool
+        """
+        if x < 0:
+            return False
+        
+        reverse_num = 0
+        temp = x
+
+        while temp != 0:
+            digit = temp % 10
+            reverse_num = reverse_num * 10 + digit
+            temp //= 10
+        
+        return reverse_num == x
+    
+
+# roman to integer
+class Solution:
+    def romanToInt(self, s):
+        m = {
+            'I': 1,
+            'V': 5,
+            'X': 10,
+            'L': 50,
+            'C': 100,
+            'D': 500,
+            'M': 1000
+        }
+        
+        ans = 0
+        
+        for i in range(len(s)):
+            if i < len(s) - 1 and m[s[i]] < m[s[i+1]]:
+                ans -= m[s[i]]
+            else:
+                ans += m[s[i]]
+        
+        return ans
