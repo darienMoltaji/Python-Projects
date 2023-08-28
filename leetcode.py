7986b2c6f391c9bec72c4874bb5007056bb1ba63
darienMoltaji <d.moltaji@gmail.com>
2023-08-28 12:24:03 -0700

    Valid parentheses uploaded

--- leetcode.py
+++ leetcode.py
@@ -41,3 +41,27 @@ class different_solution:
                 ans += m[s[i]]
         
         return ans
+
+
+
+# Valid parentheses
+
+class Solution(object):
+    def isValid(self, s):
+        """
+        :type s: str
+        :rtype: bool
+        """
+        leftSymbols = []
+        for c in s:
+            if c in ['(', '{', '[']:
+                leftSymbols.append(c)
+            elif c == ')' and len(leftSymbols) != 0 and leftSymbols[-1] == '(':
+                leftSymbols.pop()
+            elif c == '}' and len(leftSymbols) != 0 and leftSymbols[-1] == '{':
+                leftSymbols.pop()
+            elif c == ']' and len(leftSymbols) != 0 and leftSymbols[-1] == '[':
+                leftSymbols.pop()
+            else:
+                return False
+        return leftSymbols == []
