1789066215f4c0d4ed16c2e2c931abc266fdb30d
darienMoltaji <d.moltaji@gmail.com>
2023-08-31 20:23:32 -0700

    Creates remove element from array

--- leetcode.py
+++ leetcode.py
@@ -98,3 +98,20 @@ class Solution(object):
                 nums[unique] = nums[i]
                 unique += 1
         return unique
+
+
+# remove element from array of integers
+
+class Solution(object):
+    def removeElement(self, nums, val):
+        """
+        :type nums: List[int]
+        :type val: int
+        :rtype: int
+        """
+        index = 0
+        for i in range(len(nums)):
+            if nums[i] != val:
+                nums[index] = nums[i]
+                index += 1
+        return index
