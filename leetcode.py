bbf5b0382b9bd65e71333b6140f37dcf4cd15043
darienMoltaji <d.moltaji@gmail.com>
2023-09-02 21:29:32 -0700

    creates majority element algorithm

--- leetcode.py
+++ leetcode.py
@@ -156,3 +156,26 @@ class Solution(object):
             climb[i] = climb[i-1] + climb[i-2]
         print(climb)
         return climb[n]
+
+
+# majority element
+
+class Solution(object):
+    def majorityElement(self, nums):
+        """
+        :type nums: List[int]
+        :rtype: int
+        """
+        count = 0
+        majority = 0
+
+        for i in range(len(nums)):
+            if count == 0 and majority != nums[i]:
+                majority = nums[i]
+                count += 1
+            elif majority == nums[i]:
+                count += 1
+            else:
+                count -=1
+        return majority 
+        
