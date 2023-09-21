2249a7ac57205634ecb90f31f93bed4520e9f06e
darienMoltaji <d.moltaji@gmail.com>
2023-09-20 21:43:15 -0700

    divid two integers advanced

--- leetcode.py
+++ leetcode.py
@@ -208,3 +208,21 @@ class Solution(object):
 
 
 #divide two integers
+
+class Solution(object):
+    def divide(self, dividend, divisor):
+        """
+        :type dividend: int
+        :type divisor: int
+        :rtype: int
+        """
+        sign = -1 if (dividend >= 0 and divisor < 0) or (dividend < 0 and divisor >= 0) else 1
+        dividend = abs(dividend)
+        divisor = abs(divisor)
+        result = len(range(0, dividend-divisor+1, divisor))
+        if sign == -1:
+            result = -result
+        minus_limit = -(2**31)
+        plus_limit = (2**31 - 1)
+        result = min(max(result, minus_limit), plus_limit)
+        return result
